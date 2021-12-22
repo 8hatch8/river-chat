cf7bcf4052ccd36c8946049cc6c84c775a61c644
8hatch8 <yutaka.pg@gmail.com>
2021-12-22 22:53:57 +0900

    Update controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,7 @@ class ApplicationController < ActionController::Base
     end
 
     def after_sign_out_path_for(resource)
+      flash[:notice] = "ログアウトに成功しました"
       new_user_session_path
     end
 
