aa4ea98360672190748bd63ba05bfb521f97cba2
8hatch8 <yutaka.pg@gmail.com>
2021-12-22 23:00:11 +0900

    Create rooms controller and rooting

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/show'
+  root 'users#index'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
+  resources :rooms, only: [:show]
 end
